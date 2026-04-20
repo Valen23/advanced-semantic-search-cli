9f7024ae30848fdc176d17b2440899484389e964
Valen23 <valengentile23@hotmail.com>
2026-04-20 06:29:15 -0300

    fix: nullable string

--- Interfaces/ISemanticMotor.cs
+++ Interfaces/ISemanticMotor.cs
@@ -4,6 +4,6 @@ public interface ISemanticMotor
 {
     Task IngestAsync(string filePath, string folderPath);
     Task IngestFolderAsync(string folderPath);
-    Task<string> AskQuestionAsync(string question, string language, string filterTag = null);
+    Task<string> AskQuestionAsync(string question, string language, string? filterTag);
     Task DeleteDocumentAsync(string fileName);
 }
