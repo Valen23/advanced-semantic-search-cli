4319173defeb9d15e220a3c94e9ea7c72b67476b
Valen23 <valengentile23@hotmail.com>
2026-04-20 20:56:38 -0300

    feat: loop class instance

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.KernelMemory.AI.Ollama;
 using Microsoft.KernelMemory.DocumentStorage.DevTools;
 using Microsoft.KernelMemory.FileSystem.DevTools;
 using Microsoft.KernelMemory.MemoryStorage.DevTools;
+using Repl;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -28,23 +29,34 @@ if (string.IsNullOrWhiteSpace(textModel))
 if (string.IsNullOrWhiteSpace(embeddingModel))
     throw new Exception("Falta configuración crítica: 'SemanticEngine:EmbeddingModel'");
 
-if (args.Length < 2)
-{
-    Console.WriteLine("Uso de la CLI:");
-    Console.WriteLine("  Para ingerir:  dotnet run ingest <ruta_al_archivo>");
-    Console.WriteLine("  Para buscar:   dotnet run ask \"<tu_pregunta>\" [idioma_opcional]");
-    return;
-}
-
-var command = args[0].ToLower();
-var argument = args[1];
-
 var localSemanticMotor = new LocalSemanticMotor(
     storageDirectory,
     ollamaUrl,
     textModel,
     embeddingModel
 );
+
+if (args.Length == 0)
+{
+    var repl = new ReplEnvironment(localSemanticMotor);
+    await repl.StartLoopAsync();
+    return;
+}
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Uso de la CLI:");
+    Console.WriteLine("  Para ingerir archivo:   dotnet run ingest \"<ruta>\"");
+    Console.WriteLine("  Para ingerir carpeta:   dotnet run ingest-folder \"<ruta>\"");
+    Console.WriteLine(
+        "  Para buscar:            dotnet run ask \"<pregunta>\" [idioma] [category:valor]"
+    );
+    Console.WriteLine("  Para eliminar:          dotnet run delete \"<id_o_ruta>\"");
+    return;
+}
+
+var command = args[0].ToLower();
+var argument = args[1];
 
 Console.WriteLine("1. Iniciando entorno y configurando almacenamiento local...");
 
@@ -70,7 +82,9 @@ try
     }
     else
     {
-        Console.WriteLine($"Comando '{command}' no reconocido. Usa 'ingest' o 'ask'.");
+        Console.WriteLine(
+            $"Comando '{command}' no reconocido. Usa 'ingest', 'ingest-folder', 'ask' o 'delete'."
+        );
     }
 }
 catch (HttpRequestException ex)
