91fc0fd26f7954581f6464b0ab47a70e00215d1a
Valen23 <valengentile23@hotmail.com>
2026-04-21 11:35:54 -0300

    feat: streaming method

--- Interfaces/ISemanticMotor.cs
+++ Interfaces/ISemanticMotor.cs
@@ -1,10 +1,20 @@
 namespace Interfaces;
 
+public record SemanticStreamResult(
+    Microsoft.KernelMemory.SearchResult SearchResult,
+    IAsyncEnumerable<string> TextStream
+);
+
 public interface ISemanticMotor
 {
     string StorageDirectory { get; set; }
     Task IngestAsync(string filePath, string folderPath);
     Task IngestFolderAsync(string folderPath);
     Task<string> AskQuestionAsync(string question, string language, string? filterTag);
+    Task<SemanticStreamResult> AskQuestionStreamAsync(
+        string question,
+        string language,
+        string? filterTag
+    );
     Task DeleteDocumentAsync(string fileName);
 }
