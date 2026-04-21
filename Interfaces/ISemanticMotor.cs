8aa74ee4239356c152f413266f9ebf1bcc14e20c
Valen23 <valengentile23@hotmail.com>
2026-04-20 21:18:20 -0300

    feat: storage directory abstract method

--- Interfaces/ISemanticMotor.cs
+++ Interfaces/ISemanticMotor.cs
@@ -2,6 +2,7 @@ namespace Interfaces;
 
 public interface ISemanticMotor
 {
+    string StorageDirectory { get; set; }
     Task IngestAsync(string filePath, string folderPath);
     Task IngestFolderAsync(string folderPath);
     Task<string> AskQuestionAsync(string question, string language, string? filterTag);
